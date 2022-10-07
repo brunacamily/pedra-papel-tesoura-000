f22d4518a56a8e9421d8edd2ae8580f2e81a7db8
Bruna Camily <115049373+brunacamily@users.noreply.github.com>
2022-10-07 19:42:41 +0000

    VARIAÇÃO DO TRADICIONAL PEDRA, PAPEL, TESORA.

--- .ccls-cache/@home@runner@trabalho-2-pedra-papel-tesoura/main.c
+++ .ccls-cache/@home@runner@trabalho-2-pedra-papel-tesoura/main.c
@@ -4,15 +4,15 @@
 int main() {
  int escolha, aleatorio, fim;
  do{
- printf(" voce escolhe 0-PEDRA, 1-PAPEL OU 2-TESOURA ?  ");
- scanf("%d", &escolha);
- while(escolha>2||escolha<0){
- 	 printf(" voce escolhe 0-PEDRA, 1-PAPEL OU 2-TESOURA ?  ");
-	 scanf("%d", &escolha);
+ printf(" voce escolhe 0-PEDRA, 1-PAPEL, 2-TESOURA, 3-LAGARTO OU 4-SPOCK ?  ");
+ scanf("%d", &escolha); //adicionei lagarto e spock nas opicoes
+ while(escolha>4||escolha<0){ //mudei o limite de escolha>2 para escolha>4
+ 	 printf(" voce escolhe 0-PEDRA, 1-PAPEL, 2-TESOURA, 3-LAGARTO OU 4-SPOCK ?  ");
+	 scanf("%d", &escolha); 
  }
  
  srand( time(NULL) ); //semente do numero aleatório
- aleatorio = rand()%3;
+ aleatorio = rand()%5;// mudei de 3 para 5, para gerar numeros de 0 a 4
  printf(" voce:%d \n computador:%d\n", escolha, aleatorio); //gera números de 0 a 2
  
  if(escolha==0&&aleatorio==0){
@@ -59,6 +59,103 @@ int main() {
       printf(" voce: TESOURA X computador: PAPEL \n");
  		  printf(" voce ganhou! ");
 	 }
+      /*novas combinacoes  0-PEDRA, 1-PAPEL, 2-TESOURA, 3-LAGARTO OU 4-SPOCK*/
+    else{
+    if(escolha==3 && aleatorio==3 ){
+      printf(" voce: LAGARTO X computador: LAGARTO \n");
+ 		  printf(" empate! ");
+   }  
+   else{
+    if(escolha==0 && aleatorio==3 ){
+      printf(" voce: PEDRA X computador: LAGARTO \n");
+ 		  printf(" voce ganhou! ");
+   }
+   else{
+    if(escolha==3 && aleatorio==0 ){
+      printf(" voce: LAGARTO X computador: PEDRA \n");
+ 		  printf(" computador ganhou! ");
+   }
+   else{
+     if(escolha==3 && aleatorio==4 ){
+      printf(" voce: LAGARTO X computador: SPOCK \n");
+ 		  printf(" voce ganhou! ");
+  }
+  else{
+    if(escolha==4 && aleatorio==3 ){
+      printf(" voce: SPOCK X computador: LAGARTO \n");
+ 		  printf(" computador ganhou! ");
+  }
+    else{
+      if(escolha==4 && aleatorio==2 ){
+        printf(" voce: SPOCK X computador: TESOURA \n");
+ 		    printf(" voce ganhou! ");
+    }
+    else{
+      if(escolha==2 && aleatorio==4 ){
+        printf(" voce: TESOURA X computador: SPOCK \n");
+ 		    printf(" computador ganhou! ");
+    }
+    else{
+      if(escolha==2 && aleatorio==3 ){
+        printf(" voce: TESOURA X computador: LAGARTO \n");
+ 		    printf(" voce ganhou! ");
+    }
+    else{
+      if(escolha==3 && aleatorio==2 ){
+        printf(" voce: LAGARTO X computador: TESOURA \n");
+ 		    printf(" computador ganhou! ");
+    }                      
+    else{
+      if( escolha== 3 && aleatorio== 1){
+        printf(" voce: LAGARTO X computador: PAPEL \n");
+ 		    printf(" voce ganhou! ");
+    }
+    else{
+      if( escolha== 1 && aleatorio== 3){
+        printf(" voce: PAPEL X computador: LAGARTO \n");
+ 		    printf(" computador ganhou! ");
+    }
+    else{
+      if( escolha== 1 && aleatorio== 4){
+        printf(" voce: PAPEL X computador: SPOCK \n");
+ 		    printf(" voce ganhou! ");
+    }
+    else{
+      if( escolha== 4 && aleatorio== 1){
+        printf(" voce: SPOCK X computador: PAPEL \n");
+ 		    printf(" computador ganhou! ");
+    }
+    else{
+      if( escolha== 4 && aleatorio== 0){
+        printf(" voce: SPOCK X computador: PEDRA \n");
+ 		    printf(" voce ganhou! ");
+    } 
+    else{
+      if( escolha== 0 && aleatorio== 4){
+        printf(" voce: PEDRA X computador: SPOCK \n");
+ 		    printf(" computador ganhou! ");
+    }
+    else{
+      if(escolha==4&&aleatorio==4){
+        printf(" voce: SPOCK X computador: SPOCK \n");
+ 		    printf(" empate! ");
+      }
+    }
+    }
+    }
+    }
+    }
+    }
+    }
+    }
+    }
+    }
+    }
+    }
+    }
+    }
+     }
+    }
 	 }
 	 }
 	 }
